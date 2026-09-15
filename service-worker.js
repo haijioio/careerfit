662fce9deaee6e9076de5b35ef3198e4a88063dc
haijioio <1843642077@qq.com>
2026-09-15 15:22:06 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "careerfit-v5.1";
+const CACHE_NAME = "careerfit-v5.2";
 const ASSETS = ["./","./index.html","./style.css","./app.js","./manifest.json","./icon.svg","./icon-180.png","./icon-192.png","./icon-512.png"];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(()=>self.skipWaiting()));

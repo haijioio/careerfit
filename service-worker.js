d34bcfeead0edd2821c15e23202441bc33fed1cc
haijioio <1843642077@qq.com>
2026-09-24 22:03:52 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "careerfit-v6.0";
-const ASSETS = ["./","./index.html","./style.css","./app.js","./manifest.json","./icon.svg","./icon-180.png","./icon-192.png","./icon-512.png"];
+const CACHE_NAME = "careerfit-v6.1";
+const ASSETS = ["./","./index.html","./style.css","./app.js","./resume-template.html","./manifest.json","./icon.svg","./icon-180.png","./icon-192.png","./icon-512.png"];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
 });
